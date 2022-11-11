b35a7c78c97ba808fc2065e3dcf7fb25cb7beeb9
Radon Rosborough <radon@intuitiveexplanations.com>
2022-11-10 21:46:51 -0800

    Eliminate spurious uncaught exception

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,12 @@
 // chrome.action for Chrome, browser.browserAction for Firefox
 (chrome.action || browser.browserAction).onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) =>
-    chrome.tabs.sendMessage(tabs[0].id, { event: "hypercastInit" })
-  );
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    try {
+      chrome.tabs.sendMessage(tabs[0].id, { event: "hypercastInit" });
+    } catch (err) {
+      console.error(
+        "User clicked extension icon but content script was not active"
+      );
+    }
+  });
 });
